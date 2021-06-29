1a3a399cba9b992533c1b8f5502120eaa85e726b
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-29 17:35:34 +0100

    update devise log out and login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_confirmation, presence: true
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
