5b91cdfd42a016a4bb59d575cae58a5ef374b77f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-30 18:04:07 +0100

    update past and future events query implementation into scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,19 @@ class Event < ApplicationRecord
 
   has_many :event_attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :event_attendances
+
+  # def self.past_events_of(user)
+  #   user.attended_events.where(["date < ?", DateTime.now])
+  # end
+
+  # def self.future_events_of(user)
+  #   user.attended_events.where(["date > ?", DateTime.now ])
+  # end
+
+  scope :future_events_of, -> (user) { where(["date > ?", DateTime.now]) }
+
+
+  scope :past_events_of, -> (user) { where(["date < ?", DateTime.now]) }
 end
+
+
