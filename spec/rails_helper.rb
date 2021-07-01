98beb8826dbea491ba4a056ebe1dff38fdb28eb3
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-01 19:03:53 -0300

    Fix successful sign in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,23 +71,23 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   # Database Cleaner-related stuff
-  # config.before(:suite) do
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.strategy = :transaction
-  # end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
-  # config.before(:each, js: true) do
-  #   DatabaseCleaner.strategy = :truncation
-  # end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
--- spec/system/log_spec.rb
+++ spec/system/log_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe 'Signing and Loging functionality' do
-  # before :each do
-  #   User.create(name: 'Bob',
-  #     email: 'bob@example.com',
-  #     password: '123456',
-  #     password_confirmation: '123456')
-  #   end
+  before :each do
+    User.create(name: 'Bob',
+                email: 'bob@example.com',
+                password: '123456',
+                password_confirmation: '123456')
+  end
 
   describe 'the signup process', type: :feature do
     it 'registers a new user' do
       visit 'users/sign_up'
       within('#new_user') do
-        fill_in 'Name', with: 'Alice'
-        fill_in 'Email', with: 'alice@example.com'
+        fill_in 'Name', with: 'Camila'
+        fill_in 'Email', with: 'camila@example.com'
         fill_in 'Password', with: '123456'
         fill_in 'Password confirmation', with: '123456'
       end
@@ -21,11 +21,10 @@ RSpec.describe 'Signing and Loging functionality' do
   end
 
   describe 'the sign in process', type: :feature do
-
     it 'signs me in' do
       visit 'users/sign_in'
       within('#new_user') do
-        fill_in 'Email', with: 'alice@example.com'
+        fill_in 'Email', with: 'bob@example.com'
         fill_in 'Password', with: '123456'
       end
       click_button 'Log in'
