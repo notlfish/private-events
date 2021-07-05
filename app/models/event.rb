cc2128b80a2ad5c3bc4420fedb7048176a6a610c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-05 18:58:51 +0100

    remove comment on query class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,13 +7,13 @@ class Event < ApplicationRecord
   has_many :event_attendances, foreign_key: 'attended_event_id', dependent: :destroy
   has_many :attendees, through: :event_attendances
 
-  # def self.past_events_of(user)
-  #   user.attended_events.where(["date < ?", DateTime.now])
-  # end
+  def self.past_events_of(user)
+    user.attended_events.where(["date < ?", DateTime.now])
+  end
 
-  # def self.future_events_of(user)
-  #   user.attended_events.where(["date > ?", DateTime.now ])
-  # end
+  def self.future_events_of(user)
+    user.attended_events.where(["date > ?", DateTime.now ])
+  end
 
   scope :previous_events, -> { where(['date < ?', DateTime.now]) }
 
